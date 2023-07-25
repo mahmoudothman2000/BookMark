eb54e3e9e2ba486fb7d8fe5f6ed7af6d91decc9f
mahmoud.othman2000 <mahmoud.osman440@gmail.com>
2023-07-25 14:22:38 +0300

    initial commit

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,18 @@
+
 let siteNameInput = document.getElementById( "siteNameInput" );
 let siteUrlInput = document.getElementById( "siteUrlInput" );
 let btnSubmit = document.getElementById( "btnSubmit" );
 let btnClose = document.getElementById( "btnClose" );
 let btnCloseRep = document.getElementById( "btnCloseRep" );
+let btnCloseUrl = document.getElementById( "btnCloseUrl" );
+let messageValid = document.getElementById( "boxModal" );
+let messageValidUrl = document.getElementById( "boxModalUrl" );
+let messageRepeat = document.getElementById( "boxModalRepeat" );
 let urlContainerArray = [];
 
 
+
+// get Data in local Storage:
 if (JSON.parse(localStorage.getItem("urlName")) != null)
 {
   urlContainerArray = JSON.parse( localStorage.getItem( "urlName" ) );
@@ -13,36 +20,38 @@ if (JSON.parse(localStorage.getItem("urlName")) != null)
 }
 
 
+
+// Add the link in form input:
 function addUrl ()
 {
-  if ((validationInputName () === true ) && (validationInputUrl () === true ))
+  let checkResult = validFormInput();
+  if ( checkResult === true)
   {
-    let urlObject = {
+    if (checkedRepeatUrl () !== true)
+    {
+      let urlObject = {
       name: siteNameInput.value,
       url : siteUrlInput.value
-    }
-  
-    if ( checkedRepeatUrl () !== true)
-    {
+      }
       urlContainerArray.push( urlObject );
       localStorage.setItem( "urlName", JSON.stringify( urlContainerArray ) )
       displayUrl( urlContainerArray )
       clearForm ()
-    }
-    else
+    } else
     {
-      // alert( "This is URL Website Repeated" )
-      document.getElementById("boxModalRepeat").classList.remove("d-none")
+      messageRepeat.classList.remove( "d-none" );
     }
-    
   } else
   {
-    document.getElementById("boxModal").classList.remove("d-none")
+    return checkResult;
   }
+
 }
 
 
 
+
+// show the table in the document :
 function displayUrl (array)
 {
   let cartona = ``;
@@ -63,6 +72,29 @@ function displayUrl (array)
 
 
 
+
+
+// validation Data Inter ==========================================
+// valid Data Enter and not send if not valid:
+function validFormInput ()
+{
+  let regexName = /^[a-zA-Z]{3,}$/;
+  let regexUrl = /^(www.)?\w+\.[a-zA-Z]{2,}\/?$/;
+  if (regexName.test(siteNameInput.value) == false)
+  {
+    return messageValid.classList.remove("d-none")
+  } else if (regexUrl.test(siteUrlInput.value) == false)
+  {
+    return messageValidUrl.classList.remove("d-none")
+  }
+  return true
+}
+
+
+
+
+
+// change border input if valid Data:
 function validationInputName ()
 {
   let regex = /^[a-zA-Z]{3,}$/;
@@ -75,7 +107,6 @@ function validationInputName ()
     }
   return regex.test(siteNameInput.value)
 }
-
 
 function validationInputUrl ()
 {
@@ -93,6 +124,25 @@ function validationInputUrl ()
 
 
 
+
+
+
+// valid Data search if url repeat return true :
+function checkedRepeatUrl ()
+{
+  for ( let j = 0; j < urlContainerArray.length; j++)
+  {
+    return  urlContainerArray[j].url.includes(siteUrlInput.value)
+  }
+}
+// =============================================================
+
+
+
+
+
+
+// Delete link in Array by index:
 function deleteLink (index)
 {
   urlContainerArray.splice( index, 1 );
@@ -102,6 +152,9 @@ function deleteLink (index)
 
 
 
+
+
+// clear form input :
 function clearForm ()
 {
   siteNameInput.value = "";
@@ -110,56 +163,172 @@ function clearForm ()
 
 
 
+
+
+// Exit in the box Modal ===========
 btnClose.onclick = function ()
 {
-  document.getElementById( "boxModal" ).classList.add( "d-none" );
+  closeBoxModal ("boxModal")
 }
-
-
-
 
 btnCloseRep.onclick = function ()
 {
-  document.getElementById( "boxModalRepeat" ).classList.add( "d-none" );
+  closeBoxModal ("boxModalRepeat")
 }
 
-
-
-
-
-
-
-function checkedRepeatUrl ()
+btnCloseUrl.onclick = function ()
 {
-  for ( let j = 0; j < urlContainerArray.length; j++)
-  {
-    return  urlContainerArray[j].url.includes(siteUrlInput.value) 
-  }
+  closeBoxModal ( "boxModalUrl")
 }
 
+function closeBoxModal (id)
+{
+  document.getElementById( id ).classList.add( "d-none" );
+}
+// ==================================
 
 
 
 
 
-// for ( let j = 0; j < urlContainerArray.length; j++)
+
+
+// other solution @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+// let siteNameInput = document.getElementById( "siteNameInput" );
+// let siteUrlInput = document.getElementById( "siteUrlInput" );
+// let btnSubmit = document.getElementById( "btnSubmit" );
+// let btnClose = document.getElementById( "btnClose" );
+// let btnCloseRep = document.getElementById( "btnCloseRep" );
+// let urlContainerArray = [];
+
+
+// if (JSON.parse(localStorage.getItem("urlName")) != null)
 // {
-//   if (siteUrlInput.value.includes(urlContainerArray[j].url) )
-//   {
-//     alert("Dont Added ")
-//   } else
+//   urlContainerArray = JSON.parse( localStorage.getItem( "urlName" ) );
+//   displayUrl (urlContainerArray)
+// }
+
+
+// function addUrl ()
+// {
+//   if ((validationInputName () === true ) && (validationInputUrl () === true ))
 //   {
 //     let urlObject = {
 //       name: siteNameInput.value,
 //       url : siteUrlInput.value
 //     }
-
-//     urlContainerArray.unshift( urlObject );
-//     localStorage.setItem( "urlName", JSON.stringify( urlContainerArray ) )
-//     displayUrl( urlContainerArray )
-//     clearForm ()
+  
+//     if ( checkedRepeatUrl () !== true)
+//     {
+//       urlContainerArray.push( urlObject );
+//       localStorage.setItem( "urlName", JSON.stringify( urlContainerArray ) )
+//       displayUrl( urlContainerArray )
+//       clearForm ()
+//     }
+//     else
+//     {
+//       // alert( "This is URL Website Repeated" )
+//       document.getElementById("boxModalRepeat").classList.remove("d-none")
+//     }
+    
+//   } else
+//   {
+//     document.getElementById("boxModal").classList.remove("d-none")
 //   }
 // }
 
 
-console.log("www.borg.com".includes("borg.com"));
+
+// function displayUrl (array)
+// {
+//   let cartona = ``;
+//   for ( let i = 0; i < array.length; i++ )
+//   {
+//     cartona += `
+//     <tr>
+//       <td>${i}</td>
+//       <td>${array[ i ].name}</td>
+//       <td><button class="btn btn-info w-50 "> <a href="http://${array[i].url}" target="_blank"><i class="fa-solid fa-eye fs-5 me-2"></i>Visit</a></button></td>
+//       <td><button class="btn btn-danger w-50 " onclick="deleteLink(${i})"  ><i class="fa-solid fa-trash-can-arrow-up fs-5 me-1"></i> Delete</button></td>
+//     </tr>
+//     `
+//   };
+
+//   document.getElementById("tBody").innerHTML = cartona
+// }
+
+
+
+// function validationInputName ()
+// {
+//   let regex = /^[a-zA-Z]{3,}$/;
+//     if ( regex.test(siteNameInput.value))
+//     {
+//       siteNameInput.classList.add("valid")
+//     } else
+//     {
+//       siteNameInput.classList.remove( "valid" )
+//     }
+//   return regex.test(siteNameInput.value)
+// }
+
+
+// function validationInputUrl ()
+// {
+//   let regex = /^(www.)?\w+\.[a-zA-Z]{2,}\/?$/;
+
+//     if (regex.test( siteUrlInput.value ))
+//     {
+//       siteUrlInput.classList.add( "valid" )
+//     } else
+//     {
+//       siteUrlInput.classList.remove( "valid" )
+//     }
+//   return regex.test( siteUrlInput.value );
+// };
+
+
+
+// function deleteLink (index)
+// {
+//   urlContainerArray.splice( index, 1 );
+//   localStorage.setItem( "urlName", JSON.stringify( urlContainerArray ) )
+//   displayUrl (urlContainerArray)
+// }
+
+
+
+// function clearForm ()
+// {
+//   siteNameInput.value = "";
+//   siteUrlInput.value = "";
+// }
+
+
+
+// btnClose.onclick = function ()
+// {
+//   document.getElementById( "boxModal" ).classList.add( "d-none" );
+// }
+
+
+
+
+// btnCloseRep.onclick = function ()
+// {
+//   document.getElementById( "boxModalRepeat" ).classList.add( "d-none" );
+// }
+
+
+
+
+
+// function checkedRepeatUrl ()
+// {
+//   for ( let j = 0; j < urlContainerArray.length; j++)
+//   {
+//     return  urlContainerArray[j].url.includes(siteUrlInput.value)
+//   }
+// }
